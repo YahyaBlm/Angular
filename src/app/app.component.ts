f239b7c037ea848fe35a0d24f0003293380613e4
YahyaBlm <yahya.belmou@gmail.com>
2025-03-19 13:54:00 +0100

    quest 03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ArticleListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/model/article.model.ts
@@ -0,0 +1,13 @@
+
+
+export interface Article {
+    id: number;            
+    title: string;
+    content: string;
+    image: string;
+    createdAt: Date;
+    isPublished: boolean;
+    likeCount: number;
+    categoryName: string;
+    isLiked: boolean;      
+  }
